9c675bd5a9582d6163503ed1b09828e582bfd2f2
Nicholas1232 <newdinosaur8424@gmail.com>
2023-12-07 08:06:25 -0600

    Create 134.py

---
+++ 134.py
@@ -0,0 +1,22 @@
+
+
+
+
+def main():
+    
+    name = 0
+    describe = 0
+    
+    name = (input("what is your name?: "))
+    
+    describe = (input("what is a sentence you would describe yourself with?: "))
+    
+    print(name)
+    print(describe)
+    
+    
+    
+    
+    
+    
+main()
